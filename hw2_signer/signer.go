b9bbc99a73d29e6c8c38fb79b80bd918784857f6
Semyon Zernov <sem.z@protonmail.com>
2020-03-01 23:24:39 +0300

    Finished hw2

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -3,52 +3,116 @@ package main
 import (
 	"fmt"
 	"sort"
-	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
+// SingleHash evaluates hash of given data
 func SingleHash(in, out chan interface{}) {
-	data := <-in
-	str := fmt.Sprintf("%v", data)
-	result := DataSignerCrc32(str)
-	result += "~"
-	result += DataSignerCrc32(DataSignerMd5(str))
-	out <- result
-}
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 
-func MultiHash(in, out chan interface{}) {
-	data := <-in
-	str := fmt.Sprintf("%v", data)
-	var result string
-	for i := 0; i < 6; i++ {
-		result += DataSignerCrc32(strconv.Itoa(i) + str)
+	for data := range in {
+		str := fmt.Sprintf("%v", data)
+		wg.Add(1)
+		go func(data string, out chan<- interface{}, wg *sync.WaitGroup) {
+			first := crc(data)
+			second := crc(md(data, mu))
+
+			out <- <-first + "~" + <-second
+
+			wg.Done()
+		}(str, out, wg)
 	}
-	out <- result
+
+	wg.Wait()
 }
 
+// MultiHash evaluates hash of given data
+func MultiHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+
+	for data := range in {
+		wg.Add(1)
+
+		go func(data string, out chan<- interface{}, wg *sync.WaitGroup) {
+			var resultCh []chan string
+			for i := 0; i < 6; i++ {
+				resultCh = append(resultCh, crc(fmt.Sprintf("%v", i)+data))
+			}
+
+			var result string
+			for _, resultCh := range resultCh {
+				result += <-resultCh
+			}
+
+			out <- result
+
+			wg.Done()
+		}(data.(string), out, wg)
+	}
+
+	wg.Wait()
+}
+
+// CombineResults combines given data
 func CombineResults(in, out chan interface{}) {
 	var data []string
-	for i := range in {
-		data = append(data, fmt.Sprintf("%v", i))
+
+	for v := range in {
+		data = append(data, v.(string))
 	}
+
 	sort.Strings(data)
 	result := strings.Join(data, "_")
+
+	fmt.Println("result: ", result)
 	out <- result
 }
 
+// ExecutePipeline provides pipeline processing for worker functions
 func ExecutePipeline(jobs ...job) {
-	in := make(chan interface{}, 100)
-	out := make(chan interface{}, 100)
+	in := make(chan interface{})
+
+	wg := &sync.WaitGroup{}
+
+	for _, j := range jobs {
+		wg.Add(1)
+		out := make(chan interface{})
+
+		go func(j job, in, out chan interface{}, wg *sync.WaitGroup) {
+			j(in, out)
+			close(out)
+			wg.Done()
+		}(j, in, out, wg)
+
+		in = out
+	}
+
+	wg.Wait()
+}
+
+func crc(data string) chan string {
+	out := make(chan string)
+
+	go func(data string, out chan<- string) {
+		out <- DataSignerCrc32(data)
+	}(data, out)
+
+	return out
+}
+
+func md(data string, mu *sync.Mutex) string {
+	mu.Lock()
+	out := DataSignerMd5(data)
+	mu.Unlock()
+
+	return out
 }
 
 func main() {
-	// 	data := []int{0, 1}
-	// 	start := time.Now()
-	// 	ExecutePipeline(data)
-	// 	end := time.Since(start)
-	// 	fmt.Println("time: ", end)
 	inputData := []int{0, 1}
-	result := "123"
 
 	hashSignJobs := []job{
 		job(func(in, out chan interface{}) {
@@ -59,16 +123,11 @@ func main() {
 		job(SingleHash),
 		job(MultiHash),
 		job(CombineResults),
-		job(func(in, out chan interface{}) {
-			dataRaw := <-in
-			data, ok := dataRaw.(string)
-			if !ok {
-				fmt.Println("error")
-			}
-			result = data
-		}),
 	}
 
+	start := time.Now()
 	ExecutePipeline(hashSignJobs...)
-	fmt.Println(result)
+	end := time.Since(start)
+
+	fmt.Println("time: ", end)
 }
