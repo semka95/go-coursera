4dbf53f5ad8a5fe5f59e32d489cb62e9cd5aebdf
Semyon Zernov <sem.z@protonmail.com>
2020-03-09 12:15:18 +0300

    hw6 started db explorer

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"database/sql"
+	"net/http"
+)
+
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+
+type Handler struct {
+	DB *sql.DB
+}
+
+func NewDbExplorer(db *sql.DB) (http.Handler, error) {
+	mux := http.NewServeMux()
+	handler := &Handler{DB: db}
+	mux.Handle("/", handler)
+	return mux, nil
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.handleGet(w, r)
+	case http.MethodPut:
+		h.handlePut(w, r)
+	case http.MethodPost:
+		h.handlePost(w, r)
+	case http.MethodDelete:
+		h.handleDelete(w, r)
+	default:
+		http.Error(w, `{"error": "unknown method"}`, http.StatusNotFound)
+	}
+
+}
+
+func (h *Handler) handleGet(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handlePut(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handlePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handleDelete(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ hw6_db_explorer/go.mod
@@ -0,0 +1,9 @@
+module github.com/semka95/go-coursera/tree/master/hw6_db_explorer
+
+go 1.14
+
+require (
+	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.7.4 // indirect
+)
