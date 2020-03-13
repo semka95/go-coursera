8009042be07756655313420c5ae6f0d39fbc0f04
Semyon Zernov <sem.z@protonmail.com>
2020-03-13 23:46:08 +0300

    hw7 created working server function, passed couple tests, working with acl interceptors

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/tap"
 	"net"
 
 	"google.golang.org/grpc"
@@ -15,13 +18,13 @@ type AdminManager struct {
 }
 
 func (srv *BizManager) Check(ctx context.Context, in *Nothing) (*Nothing, error) {
-	return &Nothing{Dummy:true}, nil
+	return &Nothing{Dummy: true}, nil
 }
 func (srv *BizManager) Add(ctx context.Context, in *Nothing) (*Nothing, error) {
-	return &Nothing{Dummy:true}, nil
+	return &Nothing{Dummy: true}, nil
 }
 func (srv *BizManager) Test(ctx context.Context, in *Nothing) (*Nothing, error) {
-	return &Nothing{Dummy:true}, nil
+	return &Nothing{Dummy: true}, nil
 }
 
 func (srv *AdminManager) Logging(in *Nothing, server Admin_LoggingServer) error {
@@ -38,11 +41,46 @@ func StartMyMicroservice(ctx context.Context, listenAddr, ACLData string) error 
 		return err
 	}
 
-	server := grpc.NewServer()
+	var acl map[string][]string
+	err = json.Unmarshal([]byte(ACLData), &acl)
+	if err != nil {
+		return err
+	}
 
+	server := grpc.NewServer(
+		grpc.InTapHandle(statistics),
+		grpc.UnaryInterceptor(aclChecker),
+	)
 	RegisterBizServer(server, &BizManager{})
 	RegisterAdminServer(server, &AdminManager{})
 	fmt.Println("starting server at ", listenAddr)
-	server.Serve(lis)
+
+	// TODO: needs err handling https://www.atatus.com/blog/goroutines-error-handling/
+	go func() {
+		server.Serve(lis)
+	}()
+
+	go func() {
+		<-ctx.Done()
+		server.Stop()
+		lis.Close()
+	}()
+
 	return nil
+}
+
+func statistics(ctx context.Context, info *tap.Info) (context.Context, error) {
+	return ctx, nil
+}
+
+func aclChecker(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	fmt.Println(md)
+	reply, err := handler(ctx, req)
+	return reply, err
 }
