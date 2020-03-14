baa12e9a52a22f04cfebe24154079dfd97d25875
Semyon Zernov <sem.z@protonmail.com>
2020-03-14 23:51:58 +0300

    hw7 started working on logging

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -6,19 +6,33 @@ import (
 	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
+	"io"
 	"net"
 	"strings"
+	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 )
 
 type BizManager struct {
-	ACL map[string][]string
+	*Storage
 }
 
 type AdminManager struct {
-	ACL map[string][]string
+	*Storage
+}
+
+type Storage struct {
+	ACL  map[string][]string
+	Logs chan *Event
+	//Stats    chan *Stat
+	LogSubs map[chan *Event]struct{}
+	//StatSubs map[chan *Event]struct{}
+	mutex    sync.Mutex
+	capacity int
 }
 
 func (srv *BizManager) Check(ctx context.Context, in *Nothing) (*Nothing, error) {
@@ -32,6 +46,28 @@ func (srv *BizManager) Test(ctx context.Context, in *Nothing) (*Nothing, error) 
 }
 
 func (srv *AdminManager) Logging(in *Nothing, server Admin_LoggingServer) error {
+	ch := make(chan *Event, 1)
+	srv.mutex.Lock()
+	srv.LogSubs[ch] = struct{}{}
+	srv.mutex.Unlock()
+
+	go func() {
+		for c := range ch {
+			err := server.Send(c)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+		}
+		//close(ch)
+		//srv.mutex.Lock()
+		//delete(srv.LogSubs, ch)
+		//srv.mutex.Unlock()
+		//return
+	}()
 	return nil
 }
 
@@ -55,8 +91,33 @@ func StartMyMicroservice(ctx context.Context, listenAddr, ACLData string) error 
 		grpc.UnaryInterceptor(aclInterceptor),
 		grpc.StreamInterceptor(streamACLInterceptor),
 	)
-	RegisterBizServer(server, &BizManager{ACL: acl})
-	RegisterAdminServer(server, &AdminManager{ACL: acl})
+
+	store := &Storage{
+		ACL:      acl,
+		Logs:     make(chan *Event, 20),
+		capacity: 1,
+		mutex:    sync.Mutex{},
+		LogSubs:  make(map[chan *Event]struct{}),
+	}
+	go func() {
+		for val := range store.Logs {
+			store.mutex.Lock()
+			for c := range store.LogSubs {
+				c <- val
+			}
+			store.mutex.Unlock()
+		}
+		store.mutex.Lock()
+		for c := range store.LogSubs {
+			close(c)
+			delete(store.LogSubs, c)
+		}
+		store.LogSubs = nil
+		store.mutex.Unlock()
+
+	}()
+	RegisterBizServer(server, &BizManager{store})
+	RegisterAdminServer(server, &AdminManager{store})
 	fmt.Println("starting server at ", listenAddr)
 
 	// TODO: needs err handling https://www.atatus.com/blog/goroutines-error-handling/
@@ -66,6 +127,8 @@ func StartMyMicroservice(ctx context.Context, listenAddr, ACLData string) error 
 
 	go func() {
 		<-ctx.Done()
+		close(store.Logs)
+		//close(store.Stats)
 		server.Stop()
 		lis.Close()
 	}()
@@ -81,21 +144,25 @@ func aclInterceptor(
 ) (interface{}, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
 	c := md.Get("consumer")
+	p, _ := peer.FromContext(ctx)
 	if c == nil {
 		return nil, status.Errorf(codes.Unauthenticated, "empty consumer")
 	}
 
 	consumer := c[0]
 	method := info.FullMethod
-	acl := info.Server.(*BizManager).ACL
+	manager := info.Server.(*BizManager)
 
-	valid := validate(acl, consumer, method)
+	valid := validate(manager.ACL, consumer, method)
 
 	if !valid {
 		return nil, status.Errorf(codes.Unauthenticated, "method %s for consumer %s is not allowed", method, consumer)
 	}
 
+	manager.Logs <- &Event{Timestamp: time.Now().Unix(), Consumer: consumer, Method: method, Host: p.Addr.String()}
+
 	reply, err := handler(ctx, req)
+
 	return reply, err
 }
 
@@ -107,20 +174,23 @@ func streamACLInterceptor(
 ) error {
 	md, _ := metadata.FromIncomingContext(ss.Context())
 	c := md.Get("consumer")
+	p, _ := peer.FromContext(ss.Context())
 	if c == nil {
 		return status.Errorf(codes.Unauthenticated, "empty consumer")
 	}
 
 	consumer := c[0]
 	method := info.FullMethod
-	acl := srv.(*AdminManager).ACL
+	manager := srv.(*AdminManager)
 
-	valid := validate(acl, consumer, method)
+	valid := validate(manager.ACL, consumer, method)
 
 	if !valid {
 		return status.Errorf(codes.Unauthenticated, "method %s for consumer %s is not allowed", method, consumer)
 	}
 
+	manager.Logs <- &Event{Timestamp: time.Now().Unix(), Consumer: consumer, Method: method, Host: p.Addr.String()}
+	// handle err
 	err := handler(srv, ss)
 
 	return err
