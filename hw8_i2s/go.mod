824d082a582e60a42e5ae6204f4f39c0da188e4f
Semyon Zernov <sem.z@protonmail.com>
2020-03-19 15:37:26 +0300

    hw8 added struct handling, passed first test, added nested struct handling

---
+++ hw8_i2s/go.mod
@@ -0,0 +1,6 @@
+module github.com/semka95/go-coursera/tree/master/hw8_i2s
+
+go 1.14
+
+require (
+)
--- hw8_i2s/i2s.go
+++ hw8_i2s/i2s.go
@@ -1,7 +1,48 @@
 package main
 
-/*
+import (
+	"fmt"
+	"reflect"
+)
+
 func i2s(data interface{}, out interface{}) error {
-	// todo
+	e := reflect.Indirect(reflect.Indirect(reflect.ValueOf(&out)).Elem())
+	f := data.(map[string]interface{})
+
+	for i := 0; i < e.NumField(); i++ {
+		name := e.Type().Field(i).Name
+		switch b := e.Type().Field(i).Type.Kind(); b {
+		case reflect.Int:
+			val, ok := f[name].(float64)
+			if !ok {
+				return fmt.Errorf("int err")
+			}
+			e.Field(i).SetInt(int64(val))
+		case reflect.Float64:
+			val, ok := f[name].(float64)
+			if !ok {
+				return fmt.Errorf("float err")
+			}
+			e.Field(i).SetFloat(val)
+		case reflect.String:
+			val, ok := f[name].(string)
+			if !ok {
+				return fmt.Errorf("string err")
+			}
+			e.Field(i).SetString(val)
+		case reflect.Bool:
+			val, ok := f[name].(bool)
+			if !ok {
+				return fmt.Errorf("bool err")
+			}
+			e.Field(i).SetBool(val)
+		case reflect.Struct:
+			err := i2s(f[name], e.Field(i).Addr().Interface())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
-*/
